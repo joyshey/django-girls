80eaa61232e85183b568064a043579b63c329331
joyce <jeliza021@gmail.com>
2019-01-19 14:51:01 -0800

    major changes

--- digital/views.py
+++ digital/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -8,4 +8,6 @@ def post_list(request):
 
     return render(request,'digital/post_list.html',{"posts":posts})
 
-
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'digital/post_detail.html', {'post': post})
