b1a1d10e1995757ebb0e6d47200d26e7edbda4d0
joyce <jeliza021@gmail.com>
2019-01-19 13:27:17 -0800

    added a template

--- digital/views.py
+++ digital/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'digital/post_list.html',{})
+    posts=Post.objects.order_by("-created_date")
+
+    return render(request,'digital/post_list.html',{"posts":posts})
 
 
